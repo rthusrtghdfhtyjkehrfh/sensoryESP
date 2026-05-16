5b11a8ef9eed04f472f366716558f04a526730ba
dacces <narunsur51@gmail.com>
2026-05-16 13:02:34 +0300

    Update ESP.lua

--- ESP.lua
+++ ESP.lua
@@ -1,6 +1,6 @@
 --[[
   // dacces ESP //
-  // v1.0.3 //
+  // v1.0.4 //
   // 16/5/2026 //
 
   Made with love by Dacces, Gemini 3 flash, Gemini 3.1 high / low, and Claude Sonnet 4.6
@@ -9,37 +9,14 @@
   You can easily and freely use it on your own projects. I dont care. Or even paste or skid it.
   Every script that has been usen or taken that is not ai generated, were from all open sources.
 
-  The script has no optimizing purposes in this current state. But later updates will have more optimizing updates and new changes.
-
   Inspired by:
   https://v3rm.net/threads/chatgpt-esp-by-me.28629/#post-242437 > https://raw.githubusercontent.com/emptyusesdx9/astral.rip/refs/heads/main/esp
 
-  Bugs:
-  ////Fixed: Mesh chams doesnt have double execution protection, so expect bugs with it.
-
   Credits:
-  actual people: dacceson / dacces / dabid / d  a  c  c  e  s / s0ulhook / dualesp / nocturnal
+  actual people: dacces / finobe / dualesp / nocturnal
   AIs used: gemini 3 flash, gemini 3.1 high / low, claude sonnet 4.6
   font credits
-  f1nobe
-  and dual esp guy
-
-  Supports:
-  Boxes
-  Corner boxes
-  Circle boxes
-  Names
-  Distance
-  Flags
-  Weapon ESP --supports project delta Holding
-  Skeleton ESP -- taken from nocturnal
-  Health Bar
-
-  Planning to make:
-  Offscreen arrows
-  Ammo bar
-  Armor bar
-  new skeleton esp
+  f1nobe and dual esp guy
 
   Purpose of this script?
   Proves that no ESP will be unique.
