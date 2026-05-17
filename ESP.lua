d6b3c7d0b91c827807cfe32b3816cf440b844644
dacces <narunsur51@gmail.com>
2026-05-17 12:24:08 +0300

    Update ESP.lua

--- ESP.lua
+++ ESP.lua
@@ -1,25 +1,12 @@
 --[[
-  // dacces ESP //
-  // v1.0.5 //
-  // 16/5/2026 //
-
-  Made with love by Dacces, Gemini 3 flash, Gemini 3.1 high / low, and Claude Sonnet 4.6
-  This script is free to use, and could not be sold. if you bought this script, you got scammed.
-  If you say that you OWN or MADE this script, id be not happy and be mad. don't do it. please.
-  You can easily and freely use it on your own projects. I dont care. Or even paste or skid it.
-  Every script that has been usen or taken that is not ai generated, were from all open sources.
-
-  Inspired by:
-  https://v3rm.net/threads/chatgpt-esp-by-me.28629/#post-242437 > https://raw.githubusercontent.com/emptyusesdx9/astral.rip/refs/heads/main/esp
-
-  Credits:
-  actual people: dacces / finobe / dualesp / nocturnal
-  AIs used: gemini 3 flash, gemini 3.1 high / low, claude sonnet 4.6
-  font credits
-  f1nobe and dual esp guy
-
-  Purpose of this script?
-  Proves that no ESP will be unique.
+v1.0.5
+ ██████╗ ██████╗ ███████╗███╗   ██╗███████╗███████╗██████╗ 
+██╔═══██╗██╔══██╗██╔════╝████╗  ██║██╔════╝██╔════╝██╔══██╗
+██║   ██║██████╔╝█████╗  ██╔██╗ ██║█████╗  ███████╗██████╔╝
+██║   ██║██╔═══╝ ██╔══╝  ██║╚██╗██║██╔══╝  ╚════██║██╔═══╝ 
+╚██████╔╝██║     ███████╗██║ ╚████║███████╗███████║██║     
+ ╚═════╝ ╚═╝     ╚══════╝╚═╝  ╚═══╝╚══════╝╚══════╝╚═╝     
+                                                           
 ]]
 
 if not LPH_OBFUSCATED then
