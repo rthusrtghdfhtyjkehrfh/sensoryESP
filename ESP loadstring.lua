03afb0bd617dc762388fdfaa43077863d26d0581
dacces <narunsur51@gmail.com>
2026-05-25 06:00:51 +0300

    Update ESP loadstring.lua

--- ESP loadstring.lua
+++ ESP loadstring.lua
@@ -145,7 +145,7 @@ esp:Load({
         Enabled = true,
         Size = 14,
         Color = Color3.fromRGB(255, 255, 255),
-        OrbitRadius = 0,
+        OrbitRadius = 100,
         Outline = true,
         OutlineColor = Color3.fromRGB(0, 0, 0),
     },
